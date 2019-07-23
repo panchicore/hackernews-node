bad8013e872eec283e75d00b7fd299c3463e6aa9
Luis Pallares <luis@pallares.co>
2019-07-23 14:53:28 +0300

    first commit

---
+++ src/resolvers/Query.js
@@ -0,0 +1,35 @@
+async function feed(parent, args, context, info) {
+  const where = args.filter ? {
+    OR: [
+      { description_contains: args.filter },
+      { url_contains: args.filter },
+    ],
+  } : {}
+
+  const links = await context.prisma.links({
+    where,
+    skip: args.skip,
+    first: args.first,
+    orderBy: args.orderBy
+  })
+  const count = await context.prisma
+    .linksConnection({
+      where,
+    })
+    .aggregate()
+    .count()
+  return {
+    links,
+    count,
+  }
+}
+
+async function link(parent, args, context, info) {
+  return await context.prisma.link({ id: args.id })
+}
+
+
+module.exports = {
+  feed,
+  link
+}
